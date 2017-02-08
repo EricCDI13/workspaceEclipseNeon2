481af444c43f653d6837ff1d5dc44c23a9b4a460
Eric <eric.racineux@nantes.imie.fr>
2017-02-08 19:19:16 +0100

    ok

--- TutuCDI/src/fr/imie/cours/SubstractTag.java
+++ TutuCDI/src/fr/imie/cours/SubstractTag.java
@@ -1,11 +1,35 @@
 package fr.imie.cours;
 
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 public class SubstractTag extends SimpleTagSupport {
 
-	public SubstractTag() {
-		// TODO Auto-generated constructor stub
+	private Integer a;
+	private Integer b;
+
+	public Integer getA() {
+		return a;
+	}
+
+	public void setA(Integer a) {
+		this.a = a;
+	}
+
+	public Integer getB() {
+		return b;
+	}
+
+	public void setB(Integer b) {
+		this.b = b;
+	}
+
+	@Override
+	public void doTag() throws JspException, IOException {
+		getJspContext().getOut().print(a - b);
+		super.doTag();
 	}
 
 }
